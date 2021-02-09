07176fedd1f6052137f85433e45f38c589db068c
Bokie Tarathep <tarathec@ais.co.th>
2021-02-09 23:24:26 +0700

    Method & Click Event

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title){
+            // this.title = 'Words of Randiance'
+            this.title = title
+        }
+    }  
+})
+
+app.mount('#app')
